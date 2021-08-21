15f2a322af2e1b720695d3af2597b9a4d200d7ea
ChanwooBok <88770828+ChanwooBok@users.noreply.github.com>
2021-08-21 23:56:51 +0900

    callback vs promise (return ,render)

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,45 +1,61 @@
-let videos = [
-    {title: "First Video",
-    rating: 5,
-    comments: 2,
-    createdAt: "2 minutes ago",
-    views: 1,
-    id: 1,
-    },
-    {title: "Second Video",
-    rating: 5,
-    comments: 2,
-    createdAt: "2 minutes ago",
-    views: 59,
-    id: 2,
-    },
-    {title: "Third Video",
-    rating: 5,
-    comments: 2,
-    createdAt: "2 minutes ago",
-    views: 59,
-    id: 3,
+import Video from "../models/Video.js";
+
+
+    /* 
+    < callback함수를 사용하는 javascript방식 >
+    console.log("start")
+    Video.find({}, (error, videos) => {
+        if(error){
+            return res.render("server-error")
+        }
+        return res.render("home", { pageTitle : "home", videos});
+    });
+    console.log("finished");
+
+    output 
+    : start
+      finished
+      videos:[]
+      error:null
+    */
+    
+
+//  <promise를 사용하는 방식>
+
+export const home = async(req,res) => {
+    try{
+    console.log("start ! ");
+    const videos = await Video.find({});
+    return res.render("home", {pageTitle : "Home" , videos});
+    // return은 여기서 없어도 그만. 단지, function이 render작업후 바로 종료시켜 그 밑에 로그는 실행되지 않게한다.
+    } catch(error){
+        return res.render("server-error",error);
     }
-    ];
-export const trending = (req,res) => {
-    return res.render("home", { pageTitle : "home" , videos });
-} 
+    //promise를 사용하는경우 , await가 database 서칭을 마칠때까지 기다려주기때문에,
+    // 코드의 가독성도 높아지고, 순서대로 출력 되도록한다.
+    // await는 반드시 async함수와 쓰여야한다 , error는 try~catch문에서 try에서 error발생이
+    // catch구문으로넘어가서 error정보를 준다.
+
+    // video.find 부분은 database를 search하고 나서야 render시켜야 하기 때문에 가장 마지막에
+    // 실행된다.  비록 위에 써있더라도 아래에 있는 콘솔로그가 먼저 실행되고 database 서칭이 끝나야 실행된다.
+    console.log("BUt I am the second!~~");
+    //mongoose는 database에서 이 부분을 불러올 것이다. database가 respond하면
+    //mongoose는 callback function을 수행할것이다.
+    
+};
     // home이라는 pug를 렌더링한다.
     //Failed to lookup view "home" in views directory "C:\Users\chanwoo\Documents\wetube\views"
     //error 가 발생한다. 그 이유는 pug를담은  views라는 폴더는 cwd에 위치한게 아니라 src에 있기때문이다.
 export const watch = (req,res) => {
     const { id } = req.params;
-    // const id = req.params.id 와 동일 표현.위에껀 ES6스타일
-    const video = videos[id -1] 
-    return res.render("watch", { pageTitle : `Watching ${video.title}`, video});
+    // const id = req.params.id 와 동일 표현.위에껀 ES6스타일 
+    return res.render("watch", { pageTitle : `Watching `});
 }
 export const getEdit = (req,res) => {
     const {id} = req.params;
     // req.params는 videoRouter.get("/:id(\\d+)/edit",getEdit);에서
     // id값이 주어지기 때문에 express가 알아서 req.params의 값을 id로 주는것.
-
-    const video = videos[id -1] 
-    return res.render("edit",{pageTitle: `Editing : ${video.title}` ,video});
+    return res.render("edit",{pageTitle: `Editing `});
 }
 //  화면에 form을 보여주는 역할
 export const postEdit = (req,res) => {
@@ -51,9 +67,7 @@ export const postEdit = (req,res) => {
     // 즉, form에 입력한 값은 req.body로 받아올 수 있단 의미!
     
     // console.log(potato); -> output: First video
-    videos[id-1].title = potato;
-    //videos의 video들의 title을 우리가 input값에 적어준 값(potato)으로
-    // 업데이트 해줄 수 있다. 
+ 
 
     return res.redirect(`/videos/${id}`);
     // redirect는 요청한 곳으로 이동시켜줌.
@@ -68,16 +82,6 @@ export const getUpload = (req,res) => {
 export const postUpload = (req,res) => {
     // here we will add a video to the videos array.
     const {title} = req.body;
-    const newVideo = {
-        // title:req.body.title,
-        title,
-        rating:0,
-        comments:0,
-        createdAt:"just now",
-        views:1,
-        id:videos.length+1,
-    };
-    videos.push(newVideo);
     res.redirect("/");
 } 
 export const login = (req,res) => res.send("login!");
--- src/init.js
+++ src/init.js
@@ -2,6 +2,8 @@ import "./db.js";
 import "./models/Video.js"
 import app from "./server.js"
 
+
+
 const PORT = 4000;
 
 const handleListening = () => console.log(`server listening on port http://localhost:${PORT}`);
